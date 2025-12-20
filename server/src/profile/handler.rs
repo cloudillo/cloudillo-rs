f2b8a56593a253c22b2a64c006dfba316a105d75
Szilárd Hajba <szilu@symbion.hu>
2025-12-20 13:26:43 +0100

    fix(profile): rename profile_type to type in JSON response

--- server/src/profile/handler.rs
+++ server/src/profile/handler.rs
@@ -14,6 +14,7 @@ use crate::types::ApiResponse;
 pub struct Profile {
 	pub id_tag: String,
 	pub name: String,
+	#[serde(rename = "type")]
 	pub profile_type: String,
 	pub profile_pic: Option<String>,
 	pub cover_pic: Option<String>,
